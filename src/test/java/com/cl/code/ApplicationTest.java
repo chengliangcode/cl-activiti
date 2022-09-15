aad32c3a33131f9430ffd8ac9b8251d71c9154ed
chengl <714552682@qq.com>
2022-09-15 18:31:31 +0800

    commit

--- src/test/java/com/cl/code/ApplicationTest.java
+++ src/test/java/com/cl/code/ApplicationTest.java
@@ -1,11 +1,9 @@
 package com.cl.code;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngines;
-import org.activiti.engine.RepositoryService;
-import org.activiti.engine.RuntimeService;
+import org.activiti.engine.*;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +19,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ApplicationTest {
 
     @Test
-    public void test() {
+    public void deployment() {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         RepositoryService repositoryService = processEngine.getRepositoryService();
 
@@ -33,22 +31,42 @@ public class ApplicationTest {
     }
 
     @Test
-    public void test2() {
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-        RepositoryService repositoryService = processEngine.getRepositoryService();
+    public void startProcess() {
+        //部署
+        deployment();
 
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         RuntimeService runtimeService = processEngine.getRuntimeService();
 
-
-        //        3、根据流程定义Id启动流程
+        // 3、根据流程定义Id启动流程
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("cl-shengpi");
 
         //        输出内容
         System.out.println("流程定义id：" + processInstance.getProcessDefinitionId());
         System.out.println("流程实例id：" + processInstance.getId());
         System.out.println("当前活动Id：" + processInstance.getActivityId());
+    }
 
+    @Test
+    public void task() {
+
+        //部署
+        deployment();
+
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        // 3、根据流程定义Id启动流程
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("cl-shengpi");
+
+        TaskService taskService = processEngine.getTaskService();
+
+        Task task = taskService.createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
+
+        System.out.println(task);
+
+//        taskService.complete(task.getId());
 
     }
+
 
 }
